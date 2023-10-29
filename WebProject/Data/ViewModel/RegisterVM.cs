42eeb3b51d3045c1e54fe20086d77891fc314535
Đặng Minh Hà <dangminhha5@gmail.com>
2023-10-29 22:11:22 +0700

    'final'

--- WebProject/Data/ViewModel/RegisterVM.cs
+++ WebProject/Data/ViewModel/RegisterVM.cs
@@ -5,16 +5,19 @@ namespace WebProject.Data.ViewModel
     public class RegisterVM
     {
 
-        [Display(Name = "Full name")]
+        [Display(Name = "Username")]
         [Required(ErrorMessage = "Full name is required")]
+        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Username can only contain alphabet character and number")]
         public string FullName { get; set; }
 
         [Display(Name = "Email address")]
+        [RegularExpression("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", ErrorMessage = "Invalid email")]
         [Required(ErrorMessage = "Email address is required")]
         public string EmailAddress { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [RegularExpression("^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!#$%&?@ \"]).*$", ErrorMessage = "Incorrect password format")]
         public string Password { get; set; }
 
         [Display(Name = "Confirm password")]
